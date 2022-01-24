f9ecb675c9ade1294b695c37cb199555f8bb20af
jerry88897 <minishark26@gmail.com>
2022-01-24 13:35:03 +0800

    讀存檔完成

--- src/handle.js
+++ src/handle.js
@@ -39,58 +39,58 @@ ipc.on('loginRe', function (evt, resoult) {
 
 //2
 const fs = require('fs');
+const {
+    resolve
+} = require("path");
 const menu = document.getElementById("menu");
 const sidebar = document.getElementById("sidebar");
 const main_farm = document.getElementById("main_farm");
 const getMyClass = document.getElementById("getMyClass");
 const preSelectClass = document.getElementById("preSelectClass");
 
-let tableSwitch 
-let myClassTableType=false
+let tableSwitch
+let myClassTableType = false
 async function cleanFrame() {
     main_farm.innerHTML = ""
 }
 async function preSelect() {
     await cleanFrame()
-    let preSelectList={
-        "isLock":false,
-        "preSelectBlock":[]
+    let preSelectList = {
+        "isLock": false,
+        "preSelectBlock": []
     }
-    let tableHead=["分組","啟用","發送時刻(秒)","酬載1","酬載2"]
+    let tableHead = ["分組", "啟用", "發送時刻(秒)", "酬載1", "酬載2"]
     let table = document.createElement("table");
-    table.className="preTable"
+    table.className = "preTable"
     tr = table.insertRow(-1);
-    tr.className="ptr"
+    tr.className = "ptr"
 
-    for (let i=0;i<5;i++) {
-        let th = document.createElement("th");      // TABLE HEADER.
-        th.setAttribute('id','mheader');
-        th.innerHTML = tableHead[i];
-        tr.appendChild(th);
-    }
-console.log(1)
-    fs.readFile('./src/data/preSelect.json', function (err, preSelectListSaved) {
-        return new Promise(function(resolve, reject) {
+    let preSelectPromise = new Promise(function (resolve, reject) {
+        console.log(1)
+        fs.readFile('./src/data/preSelect.json', function (err, preSelectListSaved) {
             if (err) {
                 console.log("no PRS make new file");
-                for (let i=0;i<5;i++) {
-                    let preSelectBlock ={
-                        "id":0,
-                        "enable":false,
-                        "trigger":0,
-                        "list":["",""]
-                    } 
-                    preSelectBlock.id=i;
+                for (let i = 0; i < 5; i++) {
+                    let preSelectBlock = {
+                        "id": 0,
+                        "enable": false,
+                        "trigger": 0,
+                        "list": ["", ""]
+                    }
+                    preSelectBlock.id = i;
                     preSelectList.preSelectBlock.push(preSelectBlock);
                 }
                 fs.writeFile('./src/data/preSelect.json', JSON.stringify(preSelectList), function (err) {
-                    if (err)
+                    if (err) {
                         console.log(err);
-                    else
+                        reject(err);
+                    } else {
                         console.log('make new file complete.');
+                        resolve("make new");
+                    }
                 });
-                resolve("make new");
-            }else{
+
+            } else {
                 console.log("load PRS");
                 //將二進制數據轉換為字串符
                 preSelectListSaved = preSelectListSaved.toString();
@@ -99,119 +99,142 @@ console.log(1)
                 console.log(2)
                 resolve("success");
             }
-        });
-    }).then(success => {
-        
-    })
-    console.log(3)
-    for(let i=0;i<5;i++){
-        tr = table.insertRow(-1);
-        tr.className="ptr"
+        })
+    });
 
-        let td = document.createElement("td");
-        td.className="ptd"
-        td.innerHTML=i+1;
-        tr.appendChild(td);
+    for (let i = 0; i < 5; i++) {
+        let th = document.createElement("th"); // TABLE HEADER.
+        th.setAttribute('id', 'mheader');
+        th.innerHTML = tableHead[i];
+        tr.appendChild(th);
+    }
+    preSelectPromise.then(function (success) {
+        console.log(3)
+        console.log(success)
+        for (let i = 0; i < 5; i++) {
+            tr = table.insertRow(-1);
+            tr.className = "ptr"
 
-        td = document.createElement("td");
-        td.className="ptd"
-        let checkbox=document.createElement("input");
-        checkbox.setAttribute("type","checkbox");
-        checkbox.id="checkbox"+i;
-        if(preSelectList.preSelectBlock[i].enable==true){
-            checkbox.setAttribute("checked");
-        }
-        td.appendChild(checkbox);
-        tr.appendChild(td);
-
-        td = document.createElement("td");
-        td.className="ptd"
-        let timeSelect=document.createElement("input");
-        timeSelect.id="timeSelect"+i;
-        timeSelect.setAttribute("type","number");
-        timeSelect.setAttribute("min",-120);
-        timeSelect.setAttribute("max",120);
-        timeSelect.setAttribute("step",0.01);
-        timeSelect.setAttribute("value",preSelectList.preSelectBlock[i].trigger);
-        timeSelect.className="preTime"
-        td.appendChild(timeSelect);
-        tr.appendChild(td);
-
-        for(let j=0;j<2;j++){
             let td = document.createElement("td");
-            td.className="ptd"
-            let textarea=document.createElement("textarea");
-            textarea.id="textarea"+(i*2+j);
-            textarea.className="preTextrea"
-            textarea.setAttribute("max-rows",5)
-            textarea.innerHTML=preSelectList.preSelectBlock[i].list[j];
-            td.appendChild(textarea);
+            td.className = "ptd"
+            td.innerHTML = i + 1;
             tr.appendChild(td);
-        }
-    }
-    let save = document.createElement("button");
-    save.setAttribute("type","button");
-    save.className="saveBTN";
 
-    console.log(preSelectList.isLock)
-    console.log(preSelectList)
-
-    if(preSelectList.isLock==false){
-        save.innerHTML="保存並鎖定"
-    }else{
-        save.innerHTML="解除鎖定"
-    }
-    
-    main_farm.appendChild(table);
-    main_farm.appendChild(save);
-
-    save.addEventListener('click',async function(){
-        if(preSelectList.isLock==false){
-            save.innerHTML="解除鎖定"
-            preSelectList.isLock=true;
-            for(let i=0;i<5;i++){
-                preSelectList.preSelectBlock[i].enable=document.getElementById("checkbox"+i).checked;
-                document.getElementById("checkbox"+i).disabled = true;
-                preSelectList.preSelectBlock[i].trigger=document.getElementById("timeSelect"+i).value;
-                document.getElementById("timeSelect"+i).disabled = true;
-                preSelectList.preSelectBlock[i].list[0]=document.getElementById("textarea"+(i*2)).value;
-                document.getElementById("textarea"+(i*2)).disabled = true;
-                document.getElementById("textarea"+(i*2)).className = "preTextreaDark";
-                preSelectList.preSelectBlock[i].list[1]=document.getElementById("textarea"+(i*2+1)).value;
-                document.getElementById("textarea"+(i*2+1)).disabled = true;
-                document.getElementById("textarea"+(i*2+1)).className = "preTextreaDark";
+            td = document.createElement("td");
+            td.className = "ptd"
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            checkbox.className = "preInput"
+            checkbox.id = "checkbox" + i;
+            if (preSelectList.preSelectBlock[i].enable == true) {
+                checkbox.setAttribute("checked");
             }
-            fs.writeFile('./src/data/preSelect.json', JSON.stringify(preSelectList), function (err) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log('save file complete.');
-            });
-        }else{
-            save.innerHTML="保存並鎖定"
-            preSelectList.isLock=false;
-            for(let i=0;i<5;i++){
-                document.getElementById("checkbox"+i).disabled = false;
-                document.getElementById("timeSelect"+i).disabled = false;
-                document.getElementById("textarea"+(i*2)).disabled = false;
-                document.getElementById("textarea"+(i*2)).className = "preTextrea";
-                document.getElementById("textarea"+(i*2+1)).disabled = false;
-                document.getElementById("textarea"+(i*2+1)).className = "preTextrea";
+            td.appendChild(checkbox);
+            tr.appendChild(td);
+
+            td = document.createElement("td");
+            td.className = "ptd"
+            let timeSelect = document.createElement("input");
+            timeSelect.id = "timeSelect" + i;
+            timeSelect.setAttribute("type", "number");
+            timeSelect.setAttribute("min", -120);
+            timeSelect.setAttribute("max", 120);
+            timeSelect.setAttribute("step", 0.01);
+            timeSelect.setAttribute("value", preSelectList.preSelectBlock[i].trigger);
+            timeSelect.className = "preTime  preInput"
+            td.appendChild(timeSelect);
+            tr.appendChild(td);
+
+            for (let j = 0; j < 2; j++) {
+                let td = document.createElement("td");
+                td.className = "ptd"
+                let textarea = document.createElement("textarea");
+                textarea.id = "textarea" + (i * 2 + j);
+                textarea.className = "preTextrea  preInput"
+                textarea.setAttribute("max-rows", 5)
+                textarea.innerHTML = preSelectList.preSelectBlock[i].list[j];
+                td.appendChild(textarea);
+                tr.appendChild(td);
             }
         }
+        let save = document.createElement("button");
+        save.setAttribute("type", "button");
+        save.className = "saveBTN";
+
+        console.log(preSelectList.isLock)
+
+        main_farm.appendChild(table);
+        main_farm.appendChild(save);
+
+        let preInput = document.querySelectorAll('.preInput');
+        console.log(preInput)
+        if (preSelectList.isLock == false) {
+            save.innerHTML = "保存並鎖定"
+            for (var i = 0; i < preInput.length; i++) {
+                preInput[i].disabled = false;
+            }
+            for (let i = 0; i < 5; i++) {
+                document.getElementById("textarea" + (i * 2)).className = "preTextrea";
+                document.getElementById("textarea" + (i * 2 + 1)).className = "preTextrea";
+            }
+        } else {
+            save.innerHTML = "解除鎖定"
+            for (var i = 0; i < preInput.length; i++) {
+                preInput[i].disabled = true;
+            }
+            for (let i = 0; i < 5; i++) {
+                document.getElementById("textarea" + (i * 2)).className = "preTextreaDark";
+                document.getElementById("textarea" + (i * 2 + 1)).className = "preTextreaDark";
+            }
+        }
+
+
+
+        save.addEventListener('click', async function () {
+            if (preSelectList.isLock == false) {
+                save.innerHTML = "解除鎖定"
+                preSelectList.isLock = true;
+                for (var i = 0; i < preInput.length; i++) {
+                    preInput[i].disabled = true;
+                }
+                for (let i = 0; i < 5; i++) {
+                    preSelectList.preSelectBlock[i].enable = document.getElementById("checkbox" + i).checked;
+                    preSelectList.preSelectBlock[i].trigger = document.getElementById("timeSelect" + i).value;
+                    preSelectList.preSelectBlock[i].list[0] = document.getElementById("textarea" + (i * 2)).value;
+                    document.getElementById("textarea" + (i * 2)).className = "preTextreaDark";
+                    preSelectList.preSelectBlock[i].list[1] = document.getElementById("textarea" + (i * 2 + 1)).value;
+                    document.getElementById("textarea" + (i * 2 + 1)).className = "preTextreaDark";
+                }
+                fs.writeFile('./src/data/preSelect.json', JSON.stringify(preSelectList), function (err) {
+                    if (err)
+                        console.log(err);
+                    else
+                        console.log('save file complete.');
+                });
+            } else {
+                save.innerHTML = "保存並鎖定"
+                preSelectList.isLock = false;
+                for (var i = 0; i < preInput.length; i++) {
+                    preInput[i].disabled = false;
+                }
+                for (let i = 0; i < 5; i++) {
+                    document.getElementById("textarea" + (i * 2)).className = "preTextrea";
+                    document.getElementById("textarea" + (i * 2 + 1)).className = "preTextrea";
+                }
+            }
+        })
     })
-
 }
 async function showMyClass() {
     await cleanFrame()
     let table = document.createElement("table");
-    table.className="mtable"
+    table.className = "mtable"
     let switchLabel = document.createElement("label");
     let switchInput = document.createElement("input");
     let switchSpan = document.createElement("span");
 
-    let tableHead=["動作","課程代碼","課程名稱","教師","類別","學分","已選/上限","附註"]
-    let tableKey=["ln","id","name","teacher","type","point","student","ps"]
+    let tableHead = ["動作", "課程代碼", "課程名稱", "教師", "類別", "學分", "已選/上限", "附註"]
+    let tableKey = ["ln", "id", "name", "teacher", "type", "point", "student", "ps"]
 
 
     fs.readFile('./src/data/myClass.json', function (err, myClass) {
@@ -219,68 +242,68 @@ async function showMyClass() {
             return console.log(err);
         }
         console.log("start");
-    //將二進制數據轉換為字串符
+        //將二進制數據轉換為字串符
         let myclass = myClass.toString();
-    //將字符串轉換為 JSON 對象
+        //將字符串轉換為 JSON 對象
         myclass = JSON.parse(myclass);
-        let tr = table.insertRow(-1);                   // TABLE ROW.
-        tr.className="mtr"
+        let tr = table.insertRow(-1); // TABLE ROW.
+        tr.className = "mtr"
 
         for (let key of tableHead) {
-            let th = document.createElement("th");      // TABLE HEADER.
-            th.setAttribute('id','mheader');
+            let th = document.createElement("th"); // TABLE HEADER.
+            th.setAttribute('id', 'mheader');
             th.innerHTML = key;
             tr.appendChild(th);
         }
-        for(let element of myclass){
+        for (let element of myclass) {
             tr = table.insertRow(-1);
-            tr.className="mtr"
-            for(let key of tableKey){
+            tr.className = "mtr"
+            for (let key of tableKey) {
                 let td = document.createElement("td");
-                td.className="mtd"
+                td.className = "mtd"
                 td.innerHTML = element[key];
                 tr.appendChild(td);
             }
         }
     })
 
-    switchLabel.setAttribute('class','switch');
-    switchInput.setAttribute('type','checkbox');
-    switchInput.setAttribute('checked',myClassTableType);
-    tableSwitch =switchInput;
-    switchSpan.setAttribute('class','slider round');
+    switchLabel.setAttribute('class', 'switch');
+    switchInput.setAttribute('type', 'checkbox');
+    switchInput.setAttribute('checked', myClassTableType);
+    tableSwitch = switchInput;
+    switchSpan.setAttribute('class', 'slider round');
     switchLabel.appendChild(switchInput);
     switchLabel.appendChild(switchSpan);
 
     main_farm.appendChild(table);
     main_farm.appendChild(switchLabel);
-    
+
 }
 
-preSelectClass.addEventListener('click',async function(){
+preSelectClass.addEventListener('click', async function () {
     preSelect();
 })
-getMyClass.addEventListener('click',async function(){
+getMyClass.addEventListener('click', async function () {
     //ipc.send('getMyClass');
     //console.log('getMyClass');
     showMyClass();
-    tableSwitch.addEventListener('click',async function(){
+    tableSwitch.addEventListener('click', async function () {
         console.log(tableSwitch.checked);
-        myClassTableType=tableSwitch.checked;
-        if(myClassTableType==false){
-            
-        }else{
+        myClassTableType = tableSwitch.checked;
+        if (myClassTableType == false) {
+
+        } else {
 
         }
     })
 })
 
-ipc.on("readyToShow", function (evt, myClass){
+ipc.on("readyToShow", function (evt, myClass) {
     console.log("show");
     showMyClass();
 });
 
-menu.addEventListener('click',async function(){
+menu.addEventListener('click', async function () {
     sidebar.classList.toggle("active");
     console.log("open");
 })
@@ -289,6 +312,3 @@ ipc.on('myClass', function (evt, myClass) {
     console.log(myClass);
     main_farm.innerHTML = myClass;
 });
-
-
-
