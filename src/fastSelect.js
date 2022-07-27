bbbd2e5706b380fab9efcdca45fa8029ccc5de49
jerry88897 <54393648+jerry88897@users.noreply.github.com>
2022-07-27 15:49:18 +0800

    限制快速選課數量
    限制快速選課各20個、重選5個
    修正快速選課會加入未定義課程的問題

--- src/fastSelect.js
+++ src/fastSelect.js
@@ -11,50 +11,64 @@ module.exports = {
 async function pasteSelectClass(CopiedPreSelectClass, fastSelectNo) {
   let fastSelectSave;
   return new Promise((resolve, reject) => {
-    let getFile = new Promise((resolve, reject) => {
-      fs.readFile(
-        "./src/data/fastSelect.json",
-        function (err, shoppingCartData) {
-          if (err) {
-            console.error(err);
-          } else {
-            fastSelectSave = shoppingCartData.toString();
-            fastSelectSave = JSON.parse(fastSelectSave);
-            resolve();
-            console.log("read fastSelectSave...");
-          }
-        }
-      );
-    });
-    getFile
-      .then(function () {
-        console.log(1);
-        fastSelectSave["fastSelectBlock"][fastSelectNo]["list"].push(
-          CopiedPreSelectClass
-        );
-        let fastSelectSaved = new Promise(function (resolve, reject) {
-          console.log(1);
-          fs.writeFile(
-            "./src/data/fastSelect.json",
-            JSON.stringify(fastSelectSave),
-            function (err) {
-              if (err) {
-                console.log(err);
-                reject(err);
-              } else {
-                console.log("save fastSelect complete.");
-                resolve("save fastSelect");
-              }
+    if (CopiedPreSelectClass !== "") {
+      let getFile = new Promise((resolve, reject) => {
+        fs.readFile(
+          "./src/data/fastSelect.json",
+          function (err, shoppingCartData) {
+            if (err) {
+              console.error(err);
+            } else {
+              fastSelectSave = shoppingCartData.toString();
+              fastSelectSave = JSON.parse(fastSelectSave);
+              resolve();
+              console.log("read fastSelectSave...");
             }
-          );
-        });
-        fastSelectSaved.then(function () {
-          resolve();
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
+          }
+        );
       });
+      getFile
+        .then(function () {
+          let limit = 0;
+          if (fastSelectNo < 4) {
+            limit = 20;
+          } else {
+            limit = 5;
+          }
+          if (
+            fastSelectSave["fastSelectBlock"][fastSelectNo]["list"].length <
+            limit
+          ) {
+            fastSelectSave["fastSelectBlock"][fastSelectNo]["list"].push(
+              CopiedPreSelectClass
+            );
+          }
+          let fastSelectSaved = new Promise(function (resolve, reject) {
+            console.log(1);
+            fs.writeFile(
+              "./src/data/fastSelect.json",
+              JSON.stringify(fastSelectSave),
+              function (err) {
+                if (err) {
+                  console.log(err);
+                  reject(err);
+                } else {
+                  console.log("save fastSelect complete.");
+                  resolve("save fastSelect");
+                }
+              }
+            );
+          });
+          fastSelectSaved.then(function () {
+            resolve();
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      resolve();
+    }
   });
 }
 async function deleteClass(row, column) {
